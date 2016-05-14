eea70f450cde6674490dcc41c48df150f858be3a
raghavanand98 <raghav_anand98@yahoo.com>
2016-05-14 15:13:50 +0530

    fixed width issue

--- js/main.js
+++ js/main.js
@@ -8,12 +8,15 @@ $(document).ready(function(){
 var shapeForm = function(fn, div){
   var parser = math.parser();
   parser.eval(fn);
-  //console.log(parser.eval(fn))
+  console.log(parser.eval("f(0)"));
   var polygonValues = [];
 
   for(i =0; i<div.width(); i++){
+    if(parser.eval("f(" + i + ")") != "Infinity"){
     polygonValues.push(i +"px "+ parser.eval("f(" + i + ")") + "px");
-  }
+    polygonValues.push(i +"px "+ (parser.eval("f(" + i + ")")+3) + "px");
+}
+}
 //  console.log(polygonValues.join());
   $('.shaped').css("-webkit-clip-path", "polygon(" + polygonValues.join() + ")");
 
